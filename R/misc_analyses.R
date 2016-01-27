ab25e91d609df81101e8d824bd7edfe69cc3be71
Jon Leff <leff.jonathan@gmail.com>
2016-01-27 09:50:59 -0700

    add misc analyses functions

---
+++ R/misc_analyses.R
@@ -0,0 +1,177 @@
+# mctoolsr
+
+#################
+# MISC ANALYSES #
+#################
+
+#' @title Determine taxa that are common (core) across sample types
+#' @description This function returns a data frame with taxa that meet 
+#'  proportion user thresholds and are determined to be common across multiple 
+#'  sample types.
+#' @param input The input dataset as loaded by \code{load_taxa_table()}.
+#' @param type_header Mapping file header (in quotation marks) of factor for 
+#'  which you are testing for differences.
+#' @param prop_types The proportion of sample types in which a taxon must
+#'  be present to be considered core.
+#' @param prop_reps The proportion of replicate samples within a sample type
+#'  in which a taxon must be present to be considered core.
+#' @return A data frame with each core taxon as a row. The proportion of sample
+#'  types where the taxon was present, the mean count of each 
+#'  sample type, and the corresponding taxonomy (if provided), are also 
+#'  returned.
+core_taxa = function(input, type_header, prop_types = 1, prop_reps = 0.5) {
+  # indicate taxa that are present within ≥ <prop_reps> of replicates
+  df = as.data.frame(t(input$data_loaded))
+  df$type = input$map_loaded[, type_header]
+  df_m = reshape2::melt(df, id.vars = 'type', variable.name = 'OTU_ID')
+  gt_0 = function(x) sum(x > 0)
+  by_type = dplyr::summarise(dplyr::group_by(df_m, type, OTU_ID), 
+                             reps = length(value), reps_obs = gt_0(value), 
+                             mean_val = mean(value))
+  by_type = dplyr::mutate(by_type, prop_reps_obs = reps_obs / reps, 
+                          obs = prop_reps_obs >= prop_reps)
+  # indicate taxa that are core across sample types
+  # calc proportion of sample types that observed each OTU
+  by_OTU = dplyr::summarise(dplyr::group_by(by_type, OTU_ID), 
+                            prop_sample_types = sum(obs) / length(obs))
+  # add on mean values
+  means = reshape2::dcast(by_type, OTU_ID ~ type, value.var = 'mean_val')
+  if(!identical(by_OTU$OTU_ID, means$OTU_ID)) stop('Unknown error.')
+  by_OTU = cbind(by_OTU, means[, 2:ncol(means)])
+  # return core with taxonomy (if provided)
+  core = as.data.frame(dplyr::filter(by_OTU, prop_sample_types >= prop_types))
+  if(is.null(input$taxonomy_loaded)) core else {
+    tax = input$taxonomy_loaded[match(core$OTU_ID, 
+                                      row.names(input$taxonomy_loaded)), ]
+    core$taxonomy = apply(tax, 1, paste0, collapse = '; ')
+    core
+  }
+}
+
+#' @title Calculate mean proportions of shared taxa between paired samples
+#' @description This function will calculate the mean proportion of taxa 
+#'  (i.e. OTUs) that are shared between pairs of samples in a given data set.
+#'  It has the option to calculate this value for only pairs where each sample
+#'  is of a different, specified type. For example, this is useful if you want
+#'  to calculate proportion of taxa that are shared between red and blue 
+#'  samples only and not red and red or red and green, etc.  
+#' @param input Data from load_taxa_table()
+#' @param type_header (Optional) The header label from the metadata map that
+#'  has the labels which indicate the types of samples you want to calculate 
+#'  proportions for.
+#' @param sample_types (Optional) Use with type_header to indicate which two 
+#'  sample types for which you want to calculate proportions. Supply a character
+#'  vector of length 2.
+#' @param within_cat (Optional) Specify a header label from the metadata map 
+#'  that tells the function to restrict pairs to within levels of this factor.
+#'  If included, will return a data frame instead of a vector.
+calc_prop_shared_taxa = function(input, type_header, sample_types, within_cat) {
+  calc_prop_shared_main = function(input, type_header, sample_types) {
+    # get all pairs of samples
+    pairs = as.data.frame(t(combn(colnames(input$data_loaded), 2)))
+    # pairs of certain types
+    if(!missing(type_header) & !missing(sample_types)) {
+      pairs$S1_type = 
+        input$map_loaded[match(pairs[, 1], row.names(input$map_loaded)), 
+                         type_header]
+      pairs$S2_type = 
+        input$map_loaded[match(pairs[, 2], row.names(input$map_loaded)), 
+                         type_header]
+      pairs = dplyr::filter(pairs, S1_type != S2_type, S1_type %in% sample_types, 
+                     S2_type %in% sample_types)
+      # check if missing sample type(s)
+      mis_sts = sample_types[! sample_types %in% c(levels(pairs$S1_type), 
+                                                   levels(pairs$S2_type))]
+      if(length(mis_sts) > 0) {
+        stop(paste("Missing sample type: ", mis_sts, " "))
+      }
+    } else if(missing(type_header) & missing(sample_types)) {
+      # all pairs in data set 
+    } else {
+      stop("Must use 'cat_header' and 'sample_types' together.")
+    }
+    # function to calc proportion shared for each pair
+    .prop_shared_pair = function(pair, tax_table) {
+      pair_data = tax_table[, c(pair[1], pair[2])]
+      pair_data_f = pair_data[rowSums(pair_data) > 0, ]  # filt 0s
+      no_total = nrow(pair_data_f)
+      no_shared = sum(pair_data_f[, 1] * pair_data_f[, 2] > 0)
+      prop_shared = no_shared / no_total
+      prop_shared
+    }
+    pairs$prop_shared = apply(pairs, 1, .prop_shared_pair, input$data_loaded)
+    mean(pairs$prop_shared)
+  }
+  if(missing(within_cat)) {
+    calc_prop_shared_main(input, type_header, sample_types)
+  } else {
+    props = c()
+    cat_levs = unique(input$map_loaded[, within_cat])
+    for(cat_lev in cat_levs) {
+      tmp_filt = suppressMessages(filter_data(input, within_cat, 
+                                              keep_vals = cat_lev))
+      tmp_prop = calc_prop_shared_main(tmp_filt, type_header, sample_types)
+      props = c(props, tmp_prop)
+    }
+    data.frame(cat = cat_levs, prop = props)
+  }
+}
+
+#' @title Calculate the proportion of taxa in a set of samples that are 
+#'  also observed in another sample type
+#' @param input Data from load_taxa_table()
+#' @param type_header The header label from the metadata map that has the 
+#'  labels which indicate the types of samples you want to use. 
+#' @param primary_type The sample type for which you would like to calculate 
+#'  the proportion of taxa that are also observed in the source_type.
+#' @param source_type The sample type to use as the source type.
+#' @param within_cat (Optional) Specify a header label from the metadata map 
+#'  that tells the function to restrict pairs to within levels of this factor. 
+#'  If included, will return a data frame instead of a vector.
+calc_prop_taxa_from_sample_type = function(input, type_header, primary_type, 
+                                           source_type, within_cat) {
+  .calc_prop_main = function(input, type_header, primary_type, 
+                             source_type) {
+    pairs = as.data.frame(t(combn(colnames(input$data_loaded), 2)))
+    # pairs of a certain type
+    pairs$S1_type = 
+      input$map_loaded[match(pairs[, 1], row.names(input$map_loaded)), 
+                       type_header]
+    pairs$S2_type = 
+      input$map_loaded[match(pairs[, 2], row.names(input$map_loaded)), 
+                       type_header]
+    types = c(primary_type, source_type)
+    pairs = dplyr::filter(pairs, S1_type != S2_type, 
+                          S1_type %in% types, 
+                          S2_type %in% types)
+    # check if missing sample type(s)
+    mis_sts = types[! types %in% c(levels(pairs$S1_type), levels(pairs$S2_type))]
+    if(length(mis_sts) > 0) stop(paste("Missing sample type: ", mis_sts, " "))
+    # function to calc proportion shared for each pair
+    .prop_source_in_primary = function(pair, tax_table, primary_type) {
+      pair_data = tax_table[, c(pair[1], pair[2])]
+      primary_idx = which(c(pair[3], pair[4]) == primary_type)
+      no_primary = sum(pair_data[, primary_idx] > 0)
+      no_shared = sum(pair_data[, 1] * pair_data[, 2] > 0)
+      prop_overlap = no_shared / no_primary
+      prop_overlap
+    }
+    pairs$prop_overlap = apply(pairs, 1, .prop_source_in_primary, input$data_loaded, primary_type)
+    mean(pairs$prop_overlap)
+  }
+  if(missing(within_cat)) {
+    .calc_prop_main(input, type_header, primary_type, source_type)
+  } else {
+    props = c()
+    cat_levs = unique(input$map_loaded[, within_cat])
+    for(cat_lev in cat_levs) {
+      tmp_filt = suppressMessages(filter_data(input, within_cat, 
+                                              keep_vals = cat_lev))
+      tmp_prop = .calc_prop_main(tmp_filt, type_header, primary_type, 
+                                 source_type)
+      props = c(props, tmp_prop)
+    }
+    data.frame(cat = cat_levs, prop = props)
+  }
+}
+  
---
+++ tests/testthat/test_misc_analyses.R
@@ -0,0 +1,49 @@
+library(mctoolsr)
+context("Miscellaneous analyses")
+
+test_that("Prop shared calculation is correct.", {
+  test_ds = data.frame(samp1 = c(10, 0, 0, 9, 1291, 0), 
+                       samp2 = c(0, 5, 789, 0, 0, 1), 
+                       samp3 = c(5, 6, 843, 0, 87, 4),
+                       samp4 = c(0, 0, 0, 0, 0, 101), 
+                       row.names = paste0('OTU_', seq(6)))
+  pairs = as.data.frame(t(combn(colnames(test_ds), 2)))
+  paired_data = apply(pairs, 1, function(x) test_ds[, x])
+  no_taxa = sapply(paired_data, function(x) sum(x[, 1] > 0 | x[, 2] > 0))
+  shared_taxa = sapply(paired_data, function(x) sum(x[, 1] * x[, 2] > 0))
+  prop_shared = shared_taxa / no_taxa
+  mean_prop_shared = mean(prop_shared)
+  test_input = list(data_loaded = test_ds, 
+                    map_loaded = data.frame(row.names = colnames(test_ds), 
+                                            type = c('one', 'one', 'two', 
+                                                     'two')))
+  # across all samples
+  expect_equal(calc_prop_shared_taxa(test_input), mean_prop_shared)
+  # within between sample types
+  pairs$between_types = c(FALSE, TRUE, TRUE, TRUE, TRUE, FALSE)
+  mean_prop_shared_bt = mean(prop_shared[pairs$between_types])
+  expect_equal(calc_prop_shared_taxa(test_input, 'type', c('one', 'two')), 
+               mean_prop_shared_bt)
+})
+
+
+test_that("Prop taxa of another sample type calculation is correct.", {
+  test_ds = data.frame(samp1 = c(10, 0, 0, 9, 1291, 0), 
+                       samp2 = c(0, 5, 789, 0, 0, 1), 
+                       samp3 = c(5, 6, 843, 0, 87, 4),
+                       samp4 = c(0, 0, 0, 0, 0, 101), 
+                       row.names = paste0('OTU_', seq(6)))
+  test_input = list(data_loaded = test_ds, 
+                    map_loaded = data.frame(row.names = colnames(test_ds), 
+                                            type = c('one', 'one', 'two', 
+                                                     'two')))
+  # proportion of taxa in type two is also in type one
+  # prop samp3 also in 1: 2/5
+  # prop samp3 also in 2: 3/5
+  # prop samp4 also in 1: 0/1
+  # prop samp4 also in 2: 1/1
+  expected_result = mean(c(2/5, 3/5, 0, 1/1))
+  expect_equal(calc_prop_taxa_from_sample_type(test_input, 'type', 'two', 
+                                               'one'), 
+               expected_result)
+})
